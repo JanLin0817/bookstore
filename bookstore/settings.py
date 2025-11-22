bd4b79419ba825127bfc5c15d63bc5f1ae8f6709
babySushi1212 <e8o1e8o1s7@gmail.com>
2025-11-22 00:45:12 -0500

    feat: add book list and detail

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     # Third party
-    'rest_framework',
+    # 'rest_framework',
     # local app
     "books"
 ]
